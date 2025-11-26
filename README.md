bc67a083069f08ef282b23416bb9d96b21d117ca
thatdilshad <dilshadqwerty6@gmail.com>
2025-11-26 16:25:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Github_Link: https://github.com/whoisdilshad/AdvancedRecipeAPI.git
+
+Render_Link: 
+
+## Run Instruction:
+
+-> npm init -y
+-> npm install express 
+-> node server.js
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('server is running');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
